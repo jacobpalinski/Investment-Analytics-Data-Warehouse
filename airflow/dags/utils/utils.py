34599773d91004b271ae47d426b5204ba8cb668f
jacobpalinski <kpalinski99@gmail.com>
2025-07-14 08:59:10 +0800

    read_sql + company_financials_helper

--- airflow/dags/utils/utils.py
+++ airflow/dags/utils/utils.py
@@ -1,5 +1,7 @@
 # Import necessary libraries
+import datetime
 import os
+import json
 import boto3
 import snowflake.connector
 from dotenv import load_dotenv
@@ -42,4 +44,33 @@ def s3_get_object(bucket: str, key: str) -> dict:
     s3 = boto3.client('s3', aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET"))
     return s3.get_object(Bucket=bucket, Key=key)
 
+def read_sql_file(file_path: str) -> str:
+    """
+    Reads a SQL file and returns its content.
+
+    Args:
+        file_path (str): The path to the SQL file.
+
+    Returns:
+        str: The content of the SQL file.
+    """
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def company_financials_extraction_helper(cik: str, fiscal_year: int, fiscal_quarter: int, financial_statement: str, key: str, json_path: json, financials_data: list) -> dict:
+    """
+    Helper function to extract financial data from a JSON response.
+
+    Args:
+        financial_statement (str): The type of financial statement (e.g., 'income_statement').
+        key (str): The key to extract from the JSON.
+        json_path (json): The JSON object containing the financial data.
+
+    Returns:
+        dict: A dictionary containing the extracted financial data.
+    """
+    value = json_path.get('value')
+    currency = json_path.get('unit', 'USD')
+    if value is not None:
+        financials_data.append((cik, fiscal_year, fiscal_quarter, financial_statement, key, currency, value))
 
