c3089ce036442cb6d865c1860f0b5b326136c82a
jacobpalinski <kpalinski99@gmail.com>
2025-10-29 20:32:38 +0800

    snowflake table assertions

--- airflow/dags/tests/test_integration_testing.py
+++ airflow/dags/tests/test_integration_testing.py
@@ -12,6 +12,8 @@ from dags.api_extraction.finnhub_api import FinnhubApi
 from dags.api_extraction.polygon_api import PolygonApi
 from dags.api_extraction.news_api import NewsApi
 from dags.api_extraction.reddit_api import RedditApi
+from dags.api_extraction.sec_api import SecApi
+from dags.api_extraction.fred_api import FredApi
 from utils.s3_utils import S3
 
 # Create setup for logging
@@ -83,6 +85,12 @@ class IntegrationTests:
         
         # Load data into Snowflake test table
         snowflake_client.load_to_snowflake(connection=snowflake_conn, data=company_info, table_name='COMPANY_INFORMATION_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.COMPANY_INFORMATION_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(company_info)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.COMPANY_INFORMATION_TST")
     
     def test_extract_company_news(self):
         """
@@ -108,7 +116,14 @@ class IntegrationTests:
         polygon_api_client = PolygonApi(polygon_api_key=os.getenv("POLYGON_API_KEY"))
         
         # Retrieve current ticker symbols from dim_company dimension table in Snowflake
-        tickers = snowflake_client.query_current_ciks(snowflake_conn)
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("""
+                        select 
+                        distinct 
+                        ticker_symbol
+                        from investment_analytics.core.dim_company
+                        where is_current = TRUE """)
+            tickers = [row[0] for row in cursor.fetchall()]
 
         # Filter ticker symbols for testing purposes
         ticker_symbols_filter = ['AAPL', 'ABNB', 'AAL']
@@ -141,6 +156,12 @@ class IntegrationTests:
         
         # Load data into Snowflake test table
         snowflake_client.load_to_snowflake(connection=snowflake_conn, data=company_news, table_name='COMPANY_NEWS_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.COMPANY_NEWS_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(company_news)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.COMPANY_NEWS_TST")
     
     def test_extract_non_company_news(self):
         """
@@ -204,6 +225,12 @@ class IntegrationTests:
         
         # Load data into Snowflake test table
         snowflake_client.load_to_snowflake(connection=snowflake_conn, data=non_company_news, table_name='NON_COMPANY_NEWS_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.NON_COMPANY_NEWS_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(non_company_news)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.NON_COMPANY_NEWS_TST")
     
     def test_extract_reddit_submissions(self):
         """
@@ -257,6 +284,134 @@ class IntegrationTests:
         
         # Load data into Snowflake test table
         snowflake_client.load_to_snowflake(connection=snowflake_conn, data=submissions_data, table_name='REDDIT_SUBMISSIONS_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.REDDIT_SUBMISSIONS_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(submissions_data)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.REDDIT_SUBMISSIONS_TST")
+
+    def test_financials_extraction(self):
+        """
+        Tests extract_financials from SEC API and loads it into Snowflake (test table)
+        """
+        # Load environment variables
+        load_dotenv()
+
+        # Instantiate Snowflake Client
+        snowflake_client = Snowflake(
+            user=os.getenv("SNOWFLAKE_USER"),
+            account=os.getenv("SNOWFLAKE_ACCOUNT"),
+            private_key_encoded=os.getenv("SNOWFLAKE_PRIVATE_KEY_B64")
+        )
+
+        # Create snowflake connection
+        snowflake_conn = snowflake_client.create_connection(
+        warehouse='INVESTMENT_ANALYTICS_DWH',
+        database='INVESTMENT_ANALYTICS',
+        schema='TST')
+
+        # Instantiate SEC API client
+        sec_api_client = SecApi(user_agent=os.getenv("SEC_API_USER_AGENT"))
+
+        # Retrieve current CIKs from dim_company dimension table in Snowflake
+        ciks = snowflake_client.query_current_ciks(snowflake_conn)
+
+        # Create financials data list
+        financials_data = []
+
+        for cik in ciks[:10]:
+            try:
+                # Fetch financials for the CIK
+                financials = sec_api_client.sec_data_request(cik=cik)
+
+                # Append financials items to the list            
+                parsed_financials = sec_api_client.extract_financial_data(cik=cik, response=financials)
+                financials_data.extend(parsed_financials)
+            except Exception as e:
+                logger.error(f"Error fetching financials for CIK {cik}: {e}", exc_info=True)
+        
+        # Load data into Snowflake test table
+        snowflake_client.load_to_snowflake(connection=snowflake_conn, data=financials_data, table_name='COMPANY_FINANCIALS_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.COMPANY_FINANCIALS_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(financials_data)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.COMPANY_FINANCIALS_TST")
+    
+    def test_extract_economic_indicators(self):
+        """
+        Tests extract_economic_indicators from FRED API and loads it into Snowflake (test table)
+        """
+        # Load environment variables
+        load_dotenv()
+
+        # Instantiate Snowflake Client
+        snowflake_client = Snowflake(
+            user=os.getenv("SNOWFLAKE_USER"),
+            account=os.getenv("SNOWFLAKE_ACCOUNT"),
+            private_key_encoded=os.getenv("SNOWFLAKE_PRIVATE_KEY_B64")
+        )
+
+        # Create snowflake connection
+        snowflake_conn = snowflake_client.create_connection(
+        warehouse='INVESTMENT_ANALYTICS_DWH',
+        database='INVESTMENT_ANALYTICS',
+        schema='TST')
+
+        # Instantiate FRED API client
+        fred_api_key = os.getenv("FRED_API_KEY")
+        fred_api_client = FredApi(fred_api_key=fred_api_key)
+
+        # Define the economic indicators and their corresponding series IDs
+        series = {
+            'interest_rate': 'DFF',
+            'unemployment_rate': 'UNRATE',
+            'gdp_growth_rate': 'A191RL1Q225SBEA',
+            'consumer_confidence': 'UMCSENT',
+            'consumer_price_index': 'CPIAUCSL',
+        }
+
+        # Current date information
+        today = datetime.now()
+        date = today.date()
+        year = today.year
+        month_int = today.month
+        month_name = today.strftime('%B')
+        quarter = f"Q{(month_int - 1) // 3 + 1}"
+        day = today.day
+
+        # Economic indicator data storage
+        economic_indicators = []
+
+        # Retrieve economic indicators from FRED API
+        for indicator, series_id in series.items():
+            indicator_series = fred_api_client.extract_fred_data(series_id=series_id)
+
+            if not indicator_series.empty:
+                value = indicator_series.iloc[0]
+            else:
+                logger.warning(f"No data found for {indicator}")
+                value = None
+
+            economic_indicators.append({'date': date, 'year': year, 'quarter': quarter, 'month': month_name, 'day': day, 'indicator': indicator, 'value': value})
+        
+        # Load data into Snowflake test table
+        snowflake_client.load_to_snowflake(connection=snowflake_conn, data=economic_indicators, table_name='ECONOMIC_INDICATORS_TST')
+
+        with snowflake_conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM INVESTMENT_ANALYTICS.TST.ECONOMIC_INDICATORS_TST")
+            result = cursor.fetchone()
+            assert result[0] == len(economic_indicators)
+            cursor.execute("TRUNCATE TABLE INVESTMENT_ANALYTICS.TST.ECONOMIC_INDICATORS_TST")
+    
+    def test_extract_nasdaq_listed_tickers(self)
+    
+
+    
+
+
 
 
     
