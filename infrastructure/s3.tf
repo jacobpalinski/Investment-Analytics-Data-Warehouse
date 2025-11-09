dca962f0610e5367a37d7edd1338a7bb09c59ee5
jacobpalinski <kpalinski99@gmail.com>
2025-11-09 20:38:30 +0800

    reformat + s3 bucket creation

---
+++ infrastructure/s3.tf
@@ -0,0 +1,18 @@
+locals {
+  buckets = {"investment-analytics-data-warehouse", "investment-analytics-data-warehouse-tst" }
+}
+
+resource "aws_s3_bucket" "buckets" {
+  for_each = local.buckets
+  bucket = each.value
+}
+
+resource "aws_s3_object" "metadata" {
+  for_each = aws_s3_bucket.buckets
+
+  bucket = each.key
+  key = "metadata.json"
+  content = "{}"
+
+  depends_on = [aws_s3_bucket.buckets]
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,144 +1,144 @@
 # Declare aws_access_key_id variable
 variable "aws_access_key_id" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare aws_secret variable
 variable "aws_secret" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare aws_s3_bucket_variable
 variable "aws_s3_bucket" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare aws_s3_tst_bucket variable
 variable "aws_s3_tst_bucket" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare airflow_uid variable
 variable "airflow_uid" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare polygon_api_key variable
 variable "polygon_api_key" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare finnhub_api_key variable
 variable "finnhub_api_key" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare news_api_key variable
 variable "news_api_key" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare reddit_client_id variable
 variable "reddit_client_id" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare reddit_client_secret variable
 variable "reddit_client_secret" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare reddit_user_agent variable
 variable "reddit_user_agent" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare reddit_username variable
 variable "reddit_username" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare reddit_password variable
 variable "reddit_password" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare snowflake_user variable
 variable "snowflake_user" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare snowflake_password variable
 variable "snowflake_password" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare snowflake_account variable
 variable "snowflake_account" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare snowflake_private_key_b64 variable
 variable "snowflake_private_key_b64" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare snowflake_private_key_passphrase variable
 variable "snowflake_private_key_passphrase" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare fred_api_key variable
 variable "fred_api_key" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare sec_api_user_agent variable
 variable "sec_api_user_agent" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare kafka_bootstrap_servers variable
 variable "kafka_bootstrap_servers" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare kafka_topic variable
 variable "kafka_topic" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare schema_registry_url variable
 variable "schema_registry_url" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 # Declare metabase_private_key variable
 variable "metabase_private_key" {
-  type      = string
+  type = string
   sensitive = true
 }
 
